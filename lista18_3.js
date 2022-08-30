82e222c8facb67ffcfb093a25ee95d5157e1a530
Alexandre Lipari <santuslipari@gmail.com>
2022-08-30 18:47:22 -0300

    Criando função e guardando em arrays nomes e senhas

---
+++ lista18_3.js
@@ -0,0 +1,8 @@
+var nome = []
+var senha = []
+contador = 0
+function dadosUsuario (){
+    nome[contador]= prompt("Digite um nome!")
+    senha[contador]=prompt("digite uma senha!")
+    contador++
+}
