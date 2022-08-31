a57bee862d8a460ac448d5a6b88ff4fcada37746
Alexandre Lipari <santuslipari@gmail.com>
2022-08-30 22:16:17 -0300

    Criação de função para login, receber nome e senha e retornar Comparação no array para saber se true ou false.

--- lista18_3.js
+++ lista18_3.js
@@ -1,6 +1,8 @@
 var nome = []
 var senha = []
 contador = 0
+
+dadosUsuario()
 function dadosUsuario (){
     nome[contador]= prompt("Digite um nome!")
     senha[contador]=prompt("digite uma senha!")
@@ -8,4 +10,20 @@ function dadosUsuario (){
 }
 function operacaoSolicitada (){
     return prompt("qual operação deseja fazer? (1) Cadastro (2) Login (3) Excluir (4) Encerrar")
+}
+function validarLogin(nomeParametro , senhaParametro){
+    if(procurarNoArray(nomeParametro , nome) && procurarNoArray(senhaParametro , senha)){
+        return true
+    }else{
+        return false
+    }
+
+}
+function procurarNoArray(textoParametro , arrayParametro){
+for (var index = 0; index < contador; index ++){
+    if (textoParametro == arrayParametro[index]){
+        return true
+    }
+}
+return false
 }
